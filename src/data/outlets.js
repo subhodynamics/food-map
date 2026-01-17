c8b9edd67bf4fb37e501fe3236ff1449a0264376
subhodynamics <subhadeeppathak2013@gmail.com>
2026-01-17 23:33:50 +0530

    added restros

--- src/data/outlets.js
+++ src/data/outlets.js
@@ -8,5 +8,10 @@ export const outlets = [
   {name: "Amintiri - Cafe", lat: 12.974014164339627, lng: 77.60323311832431, knownFor: "Bakery"},
   {name: "Cafe Plume", lat: 12.963943514833197, lng: 77.64042316037703, knownFor: "quality French pastries Charlie and backer is excellent for German pastries and breads"},
   {name: "Olive Street Food Cafe", lat: 12.914183140636236, lng: 77.61046320169822, knownFor: "street food type snacks"},
-  {name: "Cafe Here & Now", lat: 12.915647291693082, lng: 77.63208287115663, knownFor: "Breakfast, Dining"}
+  {name: "Cafe Here & Now", lat: 12.915647291693082, lng: 77.63208287115663, knownFor: "Breakfast, Dining"},
+  {name: "Nari & Kage",lat: 12.933869471113498, lng: 77.62839256920115, knownFor: "fresh baked cheese based treats"},
+  {name: "Dolci", lat: 12.986657080945982, lng: 77.5949498890514, knownFor: "Ravioli"},
+  {name: "Lama's Cafe", lat: 12.912714337586058, lng: 77.65208958803804, knownFor: "Thupka, Tibetan cuisine"},
+  {name: "Demitasse Coffee", lat: 12.958521343301811, lng: 77.71910859568516, knownFor: "Coffee"},
+  {name: "Casa Fresco", lat: 12.982908404920948, lng: 77.6402984332655, knownFor: "Chicken Momo"}
 ];
